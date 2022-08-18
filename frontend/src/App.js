ad47eb6509bae4f96b4b9b2354dc423ad87eb942
lokeshkeelisetti <keelisettilokesh@gmail.com>
2022-08-18 14:17:07 +0530

    Intial setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+
+import Login from './pages/Login';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <Router basename='/'>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path='/login' element={<Login />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
