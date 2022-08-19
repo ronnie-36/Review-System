9e6bf956c0b38ef5afc6a5f5c333966762e77a57
lokeshkeelisetti <keelisettilokesh@gmail.com>
2022-08-19 16:08:37 +0530

    added signup page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,19 @@
-import './App.css';
-import React from 'react'
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from "react-router-dom";
+import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import Login from './pages/Login';
-import Home from './pages/Home';
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import SignUp from "./pages/SignUp";
 
 function App() {
   return (
     <>
-      <Router basename='/'>
+      <Router basename="/">
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path='/login' element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
         </Routes>
       </Router>
     </>
